c93f3ddeaab407d0359be5f58a5736477047ea4d
skawns27 <skawns27@naver.com>
2020-02-15 15:25:24 +0900

    사진 편집 앱:firebase 환경구축까지 진행

---
+++ Facesensor/app/src/main/java/com/skawns27/facesensorapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.skawns27.facesensorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseVisionFaceDetectorOptions highAccuracyOpts =
+                new FirebaseVisionFaceDetectorOptions.Builder()
+                        .setPerformanceMode(FirebaseVisionFaceDetectorOptions.ACCURATE)
+                        .setLandmarkMode(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
+                        .setClassificationMode(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
+                        .build();
+
+    }
+}
