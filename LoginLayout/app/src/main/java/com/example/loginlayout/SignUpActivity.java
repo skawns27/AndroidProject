c1fed9f1747c092a68bb8c105f6a7d39a6b71aad
skawns27 <skawns27@naver.com>
2020-02-19 13:14:08 +0900

    20.2.19. 로그인 앱:비밀번호 찾기xml+회원가입 email,password 옵저버 추가

--- LoginLayout/app/src/main/java/com/example/loginlayout/SignUpActivity.java
+++ LoginLayout/app/src/main/java/com/example/loginlayout/SignUpActivity.java
@@ -3,6 +3,8 @@ package com.example.loginlayout;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class SignUpActivity extends AppCompatActivity {
-    CheckBox man,women;
+    CheckBox male,female;
     TextInputEditText email,password,password_check;
+    Button email_confirm;
     String input_email;
     String input_password,input_check;
-
-    protected void onCreate(Bundle savedInstaceState){
-        super.onCreate(savedInstaceState);
+    //1.회원가입 신청 중복 확인 sql->DB연동 조회 확인
+    //2.중복 확인 후 메인화면으로 돌아오고 가입완료 이벤트 창 띄우기->DB정보 추가
+    //3.
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
+        email.findViewById(R.id.email);
+        password.findViewById(R.id.password);
+        email_confirm.findViewById(R.id.check_email);
+        male.findViewById(R.id.man);
+        female.findViewById(R.id.woman);
+        //email 입력변화 확인
+        email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                input_email=s.toString();//중복 이메일 확인 메서드 추가
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
         password.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -63,4 +89,8 @@ public class SignUpActivity extends AppCompatActivity {
 
         }
     }
+    private void checkEmail(String input_email){
+
+    }
+
 }
