02ec193b685450b2045aeacb3f883ca43e966c51
skawns27 <skawns27@naver.com>
2020-03-01 07:43:48 +0900

    안드로이드 프래그먼트 실습 중: 액션바+메뉴제작 중 20.3.1- 하단 탭 제작 완료,메뉴 제작 중

---
+++ SampleTab/app/src/main/java/org/techtown/tab/FragmentCallBack.java
@@ -0,0 +1,8 @@
+package org.techtown.tab;
+
+import android.os.Bundle;
+
+public interface FragmentCallBack {
+
+    public void onFragmentSelected(int position, Bundle bundle);
+}
--- SampleTab/app/src/main/java/org/techtown/tab/MainActivity.java
+++ SampleTab/app/src/main/java/org/techtown/tab/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentHostCallback;
 
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -12,9 +15,11 @@ import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationMenu;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, FragmentCallBack {
     Toolbar toolbar;
     Fragment1 fragment1;
     Fragment2 fragment2;
@@ -24,11 +29,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar=findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        ActionBar actionBar=getSupportActionBar();
-        actionBar.setDisplayShowTitleEnabled(false);
 
         fragment1=new Fragment1();
         fragment2=new Fragment2();
@@ -65,4 +66,48 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onFragmentSelected(int position, Bundle bundle) {
+        Fragment curFragment=null;
+        if(position==0){
+            curFragment=fragment1;
+        }
+        else if(position==1){
+            curFragment=fragment2;
+        }
+        else if(position==2){
+            curFragment=fragment3;
+        }
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,curFragment).commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer=findViewById(R.id.drawer_layout);
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+
+        if (id == R.id.menu1) {
+            Toast.makeText(this, "첫 번째 메뉴 선택 됨", Toast.LENGTH_LONG).show();
+            onFragmentSelected(0, null);
+        } else if (id == R.id.menu2) {
+            Toast.makeText(this, "두 번째 메뉴 선택 됨", Toast.LENGTH_LONG).show();
+            onFragmentSelected(1, null);
+        } else if (id == R.id.menu3) {
+            Toast.makeText(this, "두 번째 메뉴 선택 됨", Toast.LENGTH_LONG).show();
+            onFragmentSelected(2, null);
+        }
+        DrawerLayout drawer=findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
