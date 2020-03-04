8889978f6a386b4022d4e85d36ff222450f248f9
skawns27 <skawns27@naver.com>
2020-03-05 07:37:06 +0900

    안드로이 네트워크 실습:20.3.5.쓰레드와 소켓 사용 코드(NULL포인터 익셉션 발생 처리 중)

---
+++ SampleSocket/app/src/main/java/org/techtown/samplesocket/MainActivity.java
@@ -0,0 +1,124 @@
+package org.techtown.samplesocket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MainActivity extends AppCompatActivity {
+    Button start_server,send_btn;
+    TextView server_result,send_result;
+    Handler handler=new Handler();
+    EditText inputData;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        start_server=findViewById(R.id.star_server);
+        send_btn=findViewById(R.id.send_btn);
+        send_result=findViewById(R.id.send_result);
+        server_result=findViewById(R.id.server_result);
+        inputData=findViewById(R.id.input_data);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        send_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String data =inputData.getText().toString();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        send(data);
+                    }
+                }).start();
+            }
+        });
+        start_server.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        startServer();
+                    }
+                }).start();
+            }
+        });
+    }
+
+    public void send(String data) {
+        try {
+            int portNumber = 2700;
+            Socket socket = new Socket("localhost", portNumber);
+            printClientLog("소켓연결");
+            ObjectOutputStream outStream = new ObjectOutputStream(socket.getOutputStream());
+            outStream.writeObject(data);
+            outStream.flush();
+            printClientLog("데이터 전송함");
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void startServer(){
+        try{
+            int portNumber=2700;
+            ServerSocket server=new ServerSocket(portNumber);
+            printServerLog("서버시작"+portNumber);
+
+            while(true){
+                Socket sock=server.accept();
+                InetAddress clientHost=sock.getLocalAddress();
+                int clientPort=sock.getPort();
+                printServerLog("클라이언트 연결 됨"+clientHost+clientPort);
+
+                ObjectInputStream inputStream=new ObjectInputStream(sock.getInputStream());
+                Object obj=inputStream.readObject();
+                printServerLog("데이터 수신:"+obj);
+
+                ObjectOutputStream outputStream=new ObjectOutputStream(sock.getOutputStream());
+                outputStream.writeObject(obj+"from server");
+                outputStream.flush();
+                printServerLog("데이터 보냄");
+
+                sock.close();
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void printClientLog(final String data) {
+        Log.d("MainActivityData",data);
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                server_result.append(data+"\n");
+            }
+        });
+    }
+
+    private void printServerLog(final String data){
+        Log.d("MainActivity",data);
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                send_result.append(data+"\n");
+            }
+        });
+    }
+}
