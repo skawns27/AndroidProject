1abf87ce76ed85051852a2bbdcb5187567e6d277
Jsub <skawns27@naver.com>
2020-03-30 21:59:11 +0900

    스터디 앱:20.3.30. 로그인 서비스,DB연결 중

--- LoginLayout/app/src/main/java/com/example/loginlayout/find_password.java
+++ LoginLayout/app/src/main/java/com/example/loginlayout/find_password.java
@@ -2,13 +2,21 @@ package com.example.loginlayout;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class find_password extends AppCompatActivity {
-    TextView name,id,ch_pw,confirm;
+    EditText name,id,pw,ch_pw;
+    TextView state_log;
+    String input_name=null;
+    String input_pw=null;
+    String input_check=null;
+    String input_id=null;
     @Override
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,12 +24,81 @@ public class find_password extends AppCompatActivity {
 
         name=findViewById(R.id.TextView_name);
         id=findViewById(R.id.TextView_email);
-        ch_pw=findViewById(R.id.c)
+        pw=findViewById(R.id.TextView_pw);
+        ch_pw=findViewById(R.id.confirm_pw);
+        state_log=findViewById(R.id.state_log);
 
+        textObv();
 
     }
+    private void pw_change(){
 
+    }
     private void textObv(){
 
+        name.addTextChangedListener(new TextWatcher() {//이름
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                input_name=s.toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        id.addTextChangedListener(new TextWatcher() {//id
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                input_id=s.toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        pw.addTextChangedListener(new TextWatcher() { //비밀번호 입력
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                input_pw=s.toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        ch_pw.addTextChangedListener(new TextWatcher() { //비밀번호 확인
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                input_check=s.toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
