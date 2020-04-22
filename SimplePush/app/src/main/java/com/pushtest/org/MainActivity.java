96551aac82940ee0f4d53f6e4c32ed5ebaf3017a
Jsub <skawns27@naver.com>
2020-04-23 08:50:36 +0900

    20.4.18. push 기능 포함 프로젝트 플렛폼 앱 개발

--- SimplePush/app/src/main/java/com/pushtest/org/MainActivity.java
+++ SimplePush/app/src/main/java/com/pushtest/org/MainActivity.java
@@ -1,6 +1,8 @@
 package com.pushtest.org;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.TextView;
@@ -10,11 +12,27 @@ import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
 
 public class MainActivity extends AppCompatActivity {
-    TextView pb_discord,;
+    TextView pb_discord;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
         FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(this,
                                                                                 new OnSuccessListener<InstanceIdResult>() {
             @Override
