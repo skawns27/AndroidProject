d7c30598a36ea94ab52b56582ca57539d4a1c33b
jsNam <skawns27@naver.com>
2020-08-22 15:35:58 +0900

    20.8.22.뷰페이져 + 액션바 모듈입니다.

---
+++ FViewpagerMoudule/app/src/main/java/com/skawns27/Viewpager/Fragment3.java
@@ -0,0 +1,16 @@
+package com.skawns27.Viewpager;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class Fragment3 extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstance){
+        ViewGroup rootView=(ViewGroup) inflater.inflate(R.layout.fragment3,container,false);
+        return rootView;
+    }
+
+}
